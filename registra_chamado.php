a41194e3fab5f80fd07dad0b81e37a2d1d7e1875
Matheus Santos <111880593+math3usSan@users.noreply.github.com>
2023-06-05 09:00:35 -0300

    registrando chamados

--- registra_chamado.php
+++ registra_chamado.php
@@ -3,14 +3,29 @@
     print_r($_POST);
     echo '</pre>';
 
-    $arquivo = fopen('arquivo.hd', 'a');
-
+    // estamos trabalhando na mensagem de texto
     $titulo = str_replace('#', '-', $_POST['titulo']);
     $categoria = str_replace('#', '-', $_POST['categoria']);
     $descricao = str_replace('#', '-', $_POST['descricao']);
 
-    $texto = implode('#', $_POST);
+    
+    // $texto = implode('#', $_POST);
 
+    $texto = $titulo . ' # ' . $categoria . ' # ' . $descricao . PHP_EOL;
+    
+    // abrindo o arquivo
+    $arquivo = fopen('arquivo.hd', 'a');
+
+    // escrevendo o texto
     fwrite($arquivo, $texto);
-    echo $texto;
+
+    //fechando o arquivo
+    fclose($arquivo);
+
+    //echo $texto;
+
+    header('location: abrir_chamado.php');
+    
+
+    
 ?>
